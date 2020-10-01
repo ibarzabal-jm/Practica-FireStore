f5dae2b9b2b72548254a0003fee678b16f341695
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-10-01 00:02:54 -0300

    Update index.ts

--- firestoredemos/src/index.ts
+++ firestoredemos/src/index.ts
@@ -1,4 +1,6 @@
 import db from "./firebase/config";
+import { retornaDocumentos } from './helpers/mostrar-documentos';
+
 
 const usuario = {
     nombre: 'Martin',
@@ -6,10 +8,67 @@ const usuario = {
     fechaNaci: 0
 }
 
-db.collection('usuarios')
-    .add( usuario )
-    .then( docRef => {
-        console.log(docRef)
-    } )
-    .catch( e => console.log('error', e))
+const usuariosRef = db.collection('usuarios');
+
+// db.collection('usuarios')
+//     .add( usuario )
+//     .then( docRef => {
+//         console.log(docRef)
+//     } )
+//     .catch( e => console.log('error', e))
+
+// usuariosRef
+//     .doc('yHzkoSbte3v1u2EoZwCh')
+//     .update({
+//         activo: false
+//     });
+
+// este elimina informacion anterior y agrega lo nuevo
+
+// usuariosRef
+//     .doc('yHzkoSbte3v1u2EoZwCh')
+//     .set({
+//         edad:50,
+//     })
+
+
+// usuariosRef
+//     .doc('yHzkoSbte3v1u2EoZwCh')
+//     .delete()
+
+
+// usuariosRef
+//     .onSnapshot( snap => {
+
+//         const usuarios: any[] = [];
+
+//         snap.forEach( snapHijo => {
+//             usuarios.push({
+//                 id: snapHijo.id,
+//                 ...snapHijo.data()
+//              })
+//         })   
+
+//         console.log(usuarios);
+//     })
+
+
+
+// usuariosRef
+//     .onSnapshot( retornaDocumentos  )
+
+// usuariosRef
+//     .get().then(retornaDocumentos)
+
+// usuariosRef.where('activo','==',false).get().then( retornaDocumentos );
+// usuariosRef.where('salario','>=',1000).get().then( retornaDocumentos );
+
+// usuariosRef.where('salario','>=',1000)
+//             .where('salario','<',1200)
+//             .get().then( retornaDocumentos );
+
+usuariosRef.where('salario','>=',1000)
+            .where('activo','==',true)
+            .get().then( retornaDocumentos );
+
 
