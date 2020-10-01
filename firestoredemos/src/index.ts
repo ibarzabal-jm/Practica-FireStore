8e2d9621c2b9a46a688d5da737fb740637c7bcad
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-10-01 00:39:35 -0300

    Update index.ts

--- firestoredemos/src/index.ts
+++ firestoredemos/src/index.ts
@@ -7,6 +7,51 @@ import { retornaDocumentos } from './helpers/mostrar-documentos';
 
 const usuariosRef = db.collection('usuarios')
 
-usuariosRef.orderBy('salario','desc')
-    .get().then( retornaDocumentos );
 
+
+const btnNext = document.createElement('button');
+btnNext.innerText='Next Page';
+document.body.append( btnNext );
+
+let firstDocument:any = null;
+let lastDocument:any = null;
+
+btnNext.addEventListener('click', ()=> {
+
+    const query = usuariosRef
+                    .orderBy('nombre')
+                    .startAfter( lastDocument )
+    
+    query.limit(2).get().then( snap =>{
+
+
+        firstDocument = snap.docs[ 0 ] || null;
+        
+        //referencia al ultimo documento
+        lastDocument = snap.docs[ snap.docs.length -1] || null;
+        retornaDocumentos(snap);
+    })
+})
+
+//btn previous
+
+const btnPrev = document.createElement('button');
+btnPrev.innerText='Previous Page';
+document.body.append( btnPrev );
+
+btnPrev.addEventListener('click', ()=> {
+
+    const query = usuariosRef
+                    .orderBy('nombre')
+                    .endBefore( firstDocument )
+    
+    query.limit(2).get().then( snap =>{
+        
+        //referencia al primerDocumento
+        firstDocument = snap.docs[ 0 ] || null;
+        lastDocument = snap.docs[ snap.docs.length -1] || null;
+        retornaDocumentos(snap);
+    })
+})
+
+btnNext.click();
